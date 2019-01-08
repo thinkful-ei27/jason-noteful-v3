0da3ffe8bd0f1148d7f13945073ebc249af614fd
Publikwerker <jason.michael.hoffman@gmail.com>
2019-01-08 12:40:25 -0800

    made a get by id scratch

---
+++ scratch/queries.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+// GET by SEARCHTERM
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+
+//     let filter = {};
+
+//     if (searchTerm) {
+//       filter.title = { $regex: searchTerm, $options: 'i' };
+//     }
+
+//     return Note.find(filter).sort({ updatedAt: 'desc' });
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+//GET BY ID
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchId = '111111111111111111111108';
+
+    return Note.findById(searchId);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect()
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
